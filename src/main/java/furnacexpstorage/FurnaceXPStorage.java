cd1427f1e174ebf5e9bf8635049b1f7b3dfd9d28
Nischhelm <nischhelm@gmail.com>
2025-12-14 01:05:11 +0100

    v1.3.0
    added fluid support, furnaces can now provide their stored xp as any fluid to various fluid using mods (thanks to a PR by roidrole)

--- src/main/java/furnacexpstorage/FurnaceXPStorage.java
+++ src/main/java/furnacexpstorage/FurnaceXPStorage.java
@@ -15,7 +15,7 @@ import java.util.Random;
 )
 public class FurnaceXPStorage {
     public static final String MODID = "furnacexpstorage";
-    public static final String VERSION = "1.2.1";
+    public static final String VERSION = "1.3.0";
     public static final String NAME = "FurnaceXPStorage";
     public static final Random RAND = new Random();
     public static final Logger LOGGER = LogManager.getLogger(NAME);

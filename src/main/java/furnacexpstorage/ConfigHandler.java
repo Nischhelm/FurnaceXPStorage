ee398ab30d10cf730cedd3a18e07d3909125227d
Nischhelm <nischhelm@gmail.com>
2025-12-14 01:02:58 +0100

    auto sync config changes ingame
    should have had that the whole time...

--- src/main/java/furnacexpstorage/ConfigHandler.java
+++ src/main/java/furnacexpstorage/ConfigHandler.java
@@ -1,6 +1,10 @@
 package furnacexpstorage;
 
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = FurnaceXPStorage.MODID)
 public class ConfigHandler {
@@ -21,5 +25,15 @@ public class ConfigHandler {
 		@Config.Comment("The conversion factor from xp to liquid xp")
 		@Config.Name("Conversion Factor")
 		public float conversionFactor = 0;
+	}
+
+	@Mod.EventBusSubscriber(modid = FurnaceXPStorage.MODID)
+	public static class EventHandler {
+		@SubscribeEvent
+		public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+			if(event.getModID().equals(FurnaceXPStorage.MODID)) {
+				ConfigManager.sync(FurnaceXPStorage.MODID, Config.Type.INSTANCE);
+			}
+		}
 	}
 }
